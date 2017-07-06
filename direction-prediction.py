0d53cc2ec9e53cc38ebbf12e1d913d42e0475b99
Charlotte Feather <charlottefeather@Charlottes-MacBook-Pro.local>
2017-07-06 10:34:58 +0100

    Added printing accuracy score and prediction

--- direction-prediction.py
+++ direction-prediction.py
@@ -18,11 +18,11 @@ SVC(C=1.0, cache_size=1000000, class_weight=None, coef0=0.0,
     decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
     max_iter=-1, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
-
-
-
-
 predictions = clf.predict(X_test)
 
 from sklearn.metrics import accuracy_score
+print('Accuracy')
 print(accuracy_score(y_test, predictions))
+
+print('Prediction')
+print(clf.predict([[1037654500,5543.3100097,5810.04599608,0.346375000000003,-0.005397]]))
