ca21c77d501449ebe8c7ea2bb5f9c78fe097fd5d
y0m0 <lixi.daniele@gmail.com>
2017-07-07 15:09:59 +0100

    Refactor get_data into small usable functions

--- get_data.py
+++ get_data.py
@@ -1,43 +1,46 @@
 import pandas as pd
-import pandas_datareader.data as pdr
-import datetime
-import fix_yahoo_finance as yf
+import pandas_datareader as pdr
+from datetime import datetime, timedelta
 
-#yf.pdr_override()
-#
-#start = datetime.datetime(2010, 1, 1)
-#
-#end = datetime.datetime(2017, 7, 4)
-#
-#ftse = pdr.get_data_yahoo("^FTSE", start, end)
-#
-#print(ftse)
-#
-#ftse.to_csv('ftse.csv', encoding='utf-8')
-#
-#gbp_usd = pdr.get_data_yahoo("GBPUSD=X", start, end)
-#
-#gbp_usd.to_csv('gbpusd.csv', encoding='utf-8')
+def get_ftse(start_date, end_date):
+    ftse = pdr.get_data_yahoo('^FTSE', start_date, end_date)
+    ftse.to_csv('ftse.csv', columns=['Adj Close', 'Volume'], encoding='utf-8')
 
+def get_gbp_usd(start_date, end_date):
+    gbp_usd = pdr.get_data_yahoo('GBPUSD=X', start_date, end_date)
+    gbp_usd.to_csv('gbp_usd.csv', columns=['Adj Close'], encoding='utf-8')
 
-ftse = pd.read_csv('ftse.csv',
-        header=0,
-        index_col=["Date"],
-        parse_dates=["Date"])
+def merge_data(ftse_csv, gbp_csv):
 
-ftse.rename(columns=lambda x: 'ftse_' + x, inplace=True)
+    ftse = pd.read_csv(ftse_csv,
+            header=0,
+            index_col=["Date"],
+            parse_dates=["Date"])
 
+    ftse.rename(columns=lambda x: 'ftse_' + x, inplace=True)
 
-gbp_usd = pd.read_csv('gbpusd.csv',
-        header=0,
-        index_col=["Date"],
-        parse_dates=["Date"],
-        )
+    gbp_usd = pd.read_csv(gbp_csv,
+            header=0,
+            index_col=["Date"],
+            parse_dates=["Date"],
+            )
 
-gbp_usd.rename(columns=lambda x: 'gbusd_' + x, inplace=True)
+    gbp_usd.rename(columns=lambda x: 'gbusd_' + x, inplace=True)
 
-#print(gbp_usd.shape)
-# print(ftse.shape)
-result = pd.concat([ftse, gbp_usd], axis=1, join_axes=[gbp_usd.index])
+    merged = pd.concat([ftse, gbp_usd], axis=1, join_axes=[gbp_usd.index])
+    merged = merged.ffill()
+    merged.to_csv('merged.csv', encoding='utf-8')
 
-print(result.shape)
+def get_data():
+    yesterday = datetime.today() - timedelta(1)
+    one_month_ago = yesterday - timedelta(days=50)
+    get_ftse(one_month_ago , yesterday)
+    get_gbp_usd(one_month_ago, yesterday)
+    merge_data('ftse.csv', 'gbp_usd.csv')
+
+if __name__ == '__main__':
+    yesterday = datetime.today() - timedelta(1)
+    one_month_ago = yesterday - timedelta(days=50)
+    get_ftse(one_month_ago , yesterday)
+    get_gbp_usd(one_month_ago, yesterday)
+    merge_data('ftse.csv', 'gbp_usd.csv')
