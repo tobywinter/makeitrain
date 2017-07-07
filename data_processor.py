5c1fe8fc516eba083ef42de1c66518a7307bd598
Kavita <kkavita92@gmail.com>
2017-07-07 12:00:44 +0100

    - Add additional data processing methods to process features from daily data - Cross-validate algorithm to increase accuracy

--- data_processor.py
+++ data_processor.py
@@ -10,23 +10,21 @@ def convert_from_CSV_to_array():
             data_array.append(row)
     return(data_array)
 
-
 def get_moving_average( days ):
     data = convert_from_CSV_to_array()
     data_for_time_period = (data[-days:])
     total_ftse_value = 0
     for array in data_for_time_period:
         total_ftse_value += array[0]
-    print(total_ftse_value/days)
+    print(total_ftse_value/days
 
 def get_ftse():
     ftse = Share('^FTSE')
     return(ftse.get_open())
 
-
 def get_gbp():
     gbp = Currency('GBPUSD')
-    return(gbp.get_rate())
+    return(gbp.get_rate
 
 def get_gbp_difference():
     data = convert_from_CSV_to_array()
@@ -35,12 +33,18 @@ def get_gbp_difference():
     return(two_day_gbp/2)
 
 
+def get_2day_net_price_difference():
+    data = convert_from_CSV_to_array()
+    data_for_time_period = data[-2:]
+    two_day_price_diff = data_for_time_period[1][1] - data_for_time_period[0][1]
+    return(two_day_price_diff)
+
+
 def get_todays_data():
     ftse_price = get_ftse()
     gbp_price = get_gbp()
     today_data = [ftse_price, gbp_price]
     return(today_data)
-
 
 def add_new_data ( today_data ):
     with open("collated_data_without_headers.csv", "a") as fp:
--- direction-prediction.py
+++ direction-prediction.py
@@ -9,7 +9,7 @@ X = array[:,0:5]
 y = array[:,5]
 
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=41)
 
 from sklearn.svm import SVC
 clf = SVC()
@@ -18,11 +18,9 @@ SVC(C=1.0, cache_size=1000000, class_weight=None, coef0=0.0,
     decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
     max_iter=-1, probability=False, random_state=None, shrinking=True,
     tol=0.001, verbose=False)
+
 predictions = clf.predict(X_test)
 
 from sklearn.metrics import accuracy_score
 print('Accuracy')
 print(accuracy_score(y_test, predictions))
-
-print('Prediction')
-print(clf.predict([[1037654500,5543.3100097,5810.04599608,0.346375000000003,-0.005397]]))
